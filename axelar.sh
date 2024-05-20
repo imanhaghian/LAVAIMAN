99cac343f7cae7d68eea8dfc8b6e58489042c70c
faezemusavi <99716082+faezemusavi@users.noreply.github.com>
2024-05-20 12:18:15 +0000

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-35d13d65-14df-453e-8967-26848fd08776/"
 
 OUTPUT_DIR="logs"
 
